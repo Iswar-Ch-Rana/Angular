c1c0d945640e6fbf29411047302341ee879a84bc
Iswar-Ch-Rana <ranabitu227@gmail.com>
2024-06-06 10:46:14 +0530

    module 19-20 Form Added and minor bug fix

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -26,6 +26,7 @@ export class RecipeDetailComponent implements OnInit, OnDestroy {
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+    this.router.navigate(['/shopping-list']);
   }
 
   onEditRecipe() {
